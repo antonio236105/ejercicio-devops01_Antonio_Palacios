8ebe8fe5d274f2c804c6676448605e1256b133fe
Antonio Palacios Hernández <palacioshernandezantonio0489@gmail.com>
2026-04-26 18:02:28 +0200

    corrección del código para pasar el Action

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
- def suma (a: int, b: int) -> int:
-        """ docstring function
-        # debe dar errores en linter
-        """
-        x = "no se usa"
-        return a + b
+"""Docstring del módulo"""
+
+def suma(a: int, b: int) -> int:
+    """Suma dos números enteros."""
+    return a + b
